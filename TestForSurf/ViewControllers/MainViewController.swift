30c1de4ed712117f284c18b98b1395b5782424e1
AndreySandler <mrdreem13@gmail.com>
2023-02-12 13:11:48 +0300

    Fixed cornerRadius on SE models and other small ones

--- TestForSurf/ViewControllers/MainViewController.swift
+++ TestForSurf/ViewControllers/MainViewController.swift
@@ -7,18 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
+    // MARK: - IBOutlets
     @IBOutlet var mainView: UIView!
     @IBOutlet var sendApplicationButton: UIButton!
     
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         mainView.layer.cornerRadius = 32
         mainView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        sendApplicationButton.layer.cornerRadius = sendApplicationButton.frame.height / 2
+    }
 }
 
